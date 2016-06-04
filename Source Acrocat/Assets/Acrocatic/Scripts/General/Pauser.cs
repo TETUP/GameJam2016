7c3d55cdfcd5390d5a61a3bc5c18a11f57755e00
AkamisGJ <mrtetex@gmail.com>
2016-06-04 20:58:40 +0200

    Pause avec la mannette

--- Source Acrocat/Assets/Acrocatic/Scripts/General/Pauser.cs
+++ Source Acrocat/Assets/Acrocatic/Scripts/General/Pauser.cs
@@ -17,7 +17,7 @@ public class Pauser : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if(Input.GetKeyDown(KeyCode.P))
+		if(Input.GetButtonDown("Pause"))
 		{
 			paused = !paused;
 		}
