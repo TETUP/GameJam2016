738df7723b86c4510c55dddfb0ae88c5922247ce
TETUP <Mr.tetu@hotmail.fr>
2016-06-04 03:47:33 +0200

    IA Controller + Scene Olilou

---
+++ ForbidenBananaPatrol/Assets/Script/BasicBot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicBot : MonoBehaviour {
+
+	GameObject objectif = null;
+	Rigidbody _rb = null;
+
+	// Use this for initialization
+	void Start () {
+		objectif = GameObject.FindGameObjectWithTag ("Banana");
+		_rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (objectif != null)
+			_rb.velocity = Vector3.Normalize(objectif.transform.position-transform.position);
+		
+		//Determine le sens du personnage
+		if (_rb.velocity.x < 0)
+			transform.localScale = new Vector3 (-1.0f, transform.localScale.y, transform.localScale.z);
+		else if (_rb.velocity.x > 0)
+			transform.localScale = new Vector3 (1.0f, transform.localScale.y, transform.localScale.z);
+	}
+
+	public void ChangeObjectif(){
+		//Observe les sortie possible
+		GameObject[] allexit = GameObject.FindGameObjectsWithTag ("Exit");
+		GameObject selectExit = null;
+		//Choisi la sortie parmi toute
+		foreach (GameObject exit in allexit) {
+
+			if (selectExit == null) {
+				selectExit = exit;
+			}
+			if (Vector3.Distance (exit.transform.position, transform.position) < Vector3.Distance (selectExit.transform.position, transform.position)) {
+				selectExit = exit;
+			}
+		}
+		//Change l'objectif du bot
+		objectif = selectExit;
+	}
+}
---
+++ ForbidenBananaPatrol/Assets/Script/TouchBanana.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchBanana : MonoBehaviour {
+
+	GameObject banana = null;
+	private bool haveBanana = false;
+	BasicBot _botscript = null;
+
+	// Use this for initialization
+	void Start () {
+		_botscript = transform.parent.GetComponent<BasicBot> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider c){
+		if (c.gameObject.tag == "Banana") {
+			banana = c.gameObject; 
+			c.transform.parent = this.transform;
+			haveBanana = true;
+			transform.localPosition = new Vector3 (0.64f, 1.14f, 0.0f);
+			_botscript.ChangeObjectif ();
+		}
+
+	}
+
+	void OnDestroy(){
+		if (haveBanana)
+			banana.transform.parent = null;
+	}
+}
