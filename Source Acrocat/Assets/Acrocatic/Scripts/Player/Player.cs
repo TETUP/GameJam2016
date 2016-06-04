265a4052330234d54d08f6eb4e58b8c467a8e55e
AkamisGJ <mrtetex@gmail.com>
2016-06-04 22:58:17 +0200

    Revert "Animation player and bot"
    This reverts commit 597e3ad36a3c53498ad927c259a15e45826f1a2c.

--- Source Acrocat/Assets/Acrocatic/Scripts/Player/Player.cs
+++ Source Acrocat/Assets/Acrocatic/Scripts/Player/Player.cs
@@ -9,8 +9,6 @@ namespace Acrocatic {
 	public class Player : MonoBehaviour {
 
 		public int numJoueur = 0; // Numeros du joueur
-		public float dgt = 8.0f;
-		public float life = 115.0f;
 		// Public variables that shouldn't be shown in the inspector.
 		[HideInInspector]
 		public float hor;							// Get the Horizontal input.
@@ -107,6 +105,7 @@ namespace Acrocatic {
 			normalRotation = transform.localRotation;
 			animator = GetComponent<Animator>();
 			gravityScale = rigidbody.gravityScale;
+			
 			// Set the frontCheck and backCheck transform parent to the player's transform.
 			frontCheckTop.transform.parent = transform;
 			frontCheckBot.transform.parent = transform;
@@ -172,9 +171,8 @@ namespace Acrocatic {
 			animator.SetFloat("ySpeed", rigidbody.velocity.y);
 			animator.SetBool ("attacking", attacking);
 
-			if (Input.GetButtonDown ("Attack" + numJoueur)) {
+			if (Input.GetButtonDown ("Attack" + numJoueur)) 
 				animator.SetTrigger ("attacking");
-			}
 			
 
 			// The player is grounded if a circle at the groundcheck position overlaps anything on the ground layer.
@@ -588,9 +586,5 @@ namespace Acrocatic {
 		}
 		// ###           ###
 		// #################
-
-		public void ApplyDamage(float dgt){
-			life -= dgt;
-		}
 	}
 }
